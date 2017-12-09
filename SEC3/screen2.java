d02dc93899dfc5aa275b83bba5ba19e218d15c63
ishagupta1591 <34322124+ishagupta1591@users.noreply.github.com>
2017-12-09 16:41:52 -0500

    Add files via upload

--- SEC3/screen2.java
+++ SEC3/screen2.java
@@ -1,178 +1,172 @@
+package sce;
+
 import java.awt.*;
 import javax.swing.*;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-
-
-  
 public class screen2 extends JPanel {
-    private boolean DEBUG = false;
-    static int memberCount=0;
-	static boolean previouslyFilled=false;
-    static JPanel panel3 = new JPanel();
-    static JTable table = new JTable();
-    static JFrame frame = new JFrame("SimpleTableDemo");
-//    static Double[] results = new Double[];
+	private boolean DEBUG = false;
+	static int memberCount = 0;
+	static boolean previouslyFilled = false;
+	static JPanel panel3 = new JPanel();
+	static JTable table = new JTable();
+	static JFrame frame = new JFrame("SimpleTableDemo");
+	// static Integer[] results = new Integer[];
 
+	public void initialize_frame(JFrame frame) {
 
-    public screen2(int x, boolean y) {
-    	super(new GridLayout(1,0));
- 
-       	memberCount = x;
+		frame.setTitle("Software engineering peer evaluation system");
+		frame.setSize(800, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(4, 1));
+		JLabel head = new JLabel("Welcome to peer evaluation system", JLabel.CENTER);
+		frame.add(Box.createRigidArea(new Dimension(3, 0)));
+		frame.add(head);
+
+	}
+
+	public screen2() {
+
+	}
+
+	public screen2(int x, boolean y) {
+		super(new GridLayout(1, 0));
+
+		memberCount = x;
 		previouslyFilled = y;
-       	System.out.println(memberCount + " " + previouslyFilled);
-    }
+		System.out.println(memberCount + " " + previouslyFilled);
+	}
 
-    public void initialize_frame (JFrame frame){
-        
-        frame.setTitle("Software engineering peer evaluation system");  
-        frame.setSize(500, 300);  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        frame.setLayout(new GridLayout(4,1));
-        JLabel head = new JLabel("Welcome to peer evaluation system",JLabel.CENTER);
-        frame.add(Box.createRigidArea(new Dimension(3,0)));
-        frame.add(head);
-        
-    }
+	public void create_table() {
+		String[] columnNames = { "#", "Name", "Professionalism", "Participation", "Work Evaluation", };
 
-
-    public double[] normalize (int [][] scores){
-        
-        double[] normalized_score = new double[scores.length];
-
-        int total = 0;
-        for (int i=0;i<scores.length;i++)
-            for(int j=0;j<scores[0].length;j++)
-                total = total + scores[i][j];
-
-
-        for(int i=0;i<scores.length;i++){
-            int current_sum = 0;
-
-            for(int j=0;j<scores[0].length; j++)
-                current_sum = current_sum + scores[i][j];
-
-
-            normalized_score[i] = current_sum / total;
-
-        }
-
-        return normalized_score;
-
-    }
-
-
-    public void create_table(){
-       	String[] columnNames = {"#","Name",
-                        "Professionalism",
-                        "Participation",
-                        "Work Evaluation",
-                        };                    
-
-
-	    Object[][] bank = {
-	    {new Integer(1),"John", new Double(5), new Double(4), new Double(5)},
-	    {new Integer(2),"Shavon", new Double(5), new Double(4), new Double(5)},
-	   	{new Integer(3),"Mike", new Double(5), new Double(4), new Double(5)},
-	   	{new Integer(4),"Gordon", new Double(5), new Double(4), new Double(5)},
-	   	{new Integer(5),"Liz", new Double(5), new Double(4), new Double(5)},
-	   	{new Integer(6),"Karen", new Double(5), new Double(4), new Double(5)},
-	   	{new Integer(7),"Rob", new Double(5), new Double(4), new Double(5)}
-		};
+		Object[][] bank = { { new Integer(1), "John", new Integer(5), new Integer(4), new Integer(5) },
+				{ new Integer(2), "Shavon", new Integer(5), new Integer(4), new Integer(5) },
+				{ new Integer(3), "Mike", new Integer(5), new Integer(4), new Integer(5) },
+				{ new Integer(4), "Gordon", new Integer(5), new Integer(4), new Integer(5) },
+				{ new Integer(5), "Liz", new Integer(5), new Integer(4), new Integer(5) },
+				{ new Integer(6), "Karen", new Integer(5), new Integer(4), new Integer(5) },
+				{ new Integer(7), "Rob", new Integer(5), new Integer(4), new Integer(5) } };
 
 		Object[][] data = new Object[memberCount][5];
 
-		if(previouslyFilled==true){
-			for (int i=0;i<memberCount;i++)
-				for(int j=0;j<5;j++){
+		Integer[] scoreList = new Integer[] { 0, 1, 2, 3, 4, 5 };
+
+		if (previouslyFilled == true) {
+			for (int i = 0; i < memberCount; i++)
+				for (int j = 0; j < 5; j++) {
 					data[i][j] = bank[i][j];
 					System.out.println(data[i][j]);
 				}
 
+		} else
+			for (int i = 0; i < memberCount; i++) {
+				data[i][0] = bank[i][0];
+				data[i][1] = bank[i][1];
+				for (int j = 2; j < 5; j++) {
+					data[i][j] = 0;
+					System.out.println(data[i][j]);
+				}
+				// System.out.println(data[i][j]);
+			}
+
+		this.table = new JTable(data, columnNames);
+		this.table.setPreferredScrollableViewportSize(new Dimension(500, 200));
+		this.table.setFillsViewportHeight(true);
+		this.table.setRowHeight(30);
+
+		for (int i = 2; i < 5; i++) {
+			JComboBox<Integer> myComboBox = new JComboBox<Integer>(scoreList);
+			this.table.getColumnModel().getColumn(i).setCellEditor(new DefaultCellEditor(myComboBox));
 		}
-        
-        this.table = new JTable(data, columnNames);
-        this.table.setPreferredScrollableViewportSize(new Dimension(500, 70));
-        this.table.setFillsViewportHeight(true);
- 
-        if (DEBUG) {
-            this.table.addMouseListener(new MouseAdapter() {
-                public void mouseClicked(MouseEvent e) {
-                    printDebugData(table);
-                }
-            });
-        }
- 
-        //Create the scroll pane and add the table to it.
-        JScrollPane scrollPane = new JScrollPane(this.table);
- 
-        //Add the scroll pane to this panel.
-        add(scrollPane);
-    }
-    
 
+		if (DEBUG) {
+			this.table.addMouseListener(new MouseAdapter() {
+				public void mouseClicked(MouseEvent e) {
+					printDebugData(table);
+				}
+			});
+		}
 
+		// Create the scroll pane and add the table to it.
+		JScrollPane scrollPane = new JScrollPane(this.table);
 
+		// Add the scroll pane to this panel.
+		add(scrollPane);
+	}
 
+	private void printDebugData(JTable table) {
+		int numRows = table.getRowCount();
+		int numCols = table.getColumnCount();
+		javax.swing.table.TableModel model = table.getModel();
 
+		System.out.println("Value of data: ");
+		for (int i = 0; i < numRows; i++) {
+			System.out.print("    row " + i + ":");
+			for (int j = 0; j < numCols; j++) {
+				System.out.print("  " + model.getValueAt(i, j));
+			}
+			System.out.println();
+		}
+		System.out.println("--------------------------");
+	}
 
+	/**
+	 * Create the GUI and show it. For thread safety, this method should be
+	 * invoked from the event-dispatching thread.
+	 */
+	public static void createAndShowGUI() {
+		// Create and set up the window.
 
+		// Create and set up the content pane.
+		screen2 obj = new screen2(memberCount, previouslyFilled);
+		obj.setOpaque(true); // content panes must be opaque
+		// frame.setContentPane(newContentPane);
+		// Display the window.
+		obj.initialize_frame(obj.frame);
+		obj.create_table();
+		obj.frame.add(table);
+		obj.frame.pack();
+		JButton submit = new JButton("Submit");
+		obj.frame.add(submit);
+		// submit.addActionListener(this);
+		obj.frame.setVisible(true);
+	}
 
+	public double[] normalize(int[][] scores) {
 
-    private void printDebugData(JTable table) {
-        int numRows = table.getRowCount();
-        int numCols = table.getColumnCount();
-        javax.swing.table.TableModel model = table.getModel();
- 
-        System.out.println("Value of data: ");
-        for (int i=0; i < numRows; i++) {
-            System.out.print("    row " + i + ":");
-            for (int j=0; j < numCols; j++) {
-                System.out.print("  " + model.getValueAt(i, j));
-            }
-            System.out.println();
-        }
-        System.out.println("--------------------------");
-    }
- 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    public static void createAndShowGUI() {
-        //Create and set up the window.
+		double[] normalized_score = new double[scores.length];
 
-        //Create and set up the content pane.
-        screen2 obj = new screen2(memberCount,previouslyFilled); // set this to 5 just for testing purposes -> this value needs to be extracted from function call
-        
-        obj.setOpaque(true); //content panes must be opaque
-//      frame.setContentPane(newContentPane);
-        //Display the window.
-        obj.initialize_frame(obj.frame);
-        obj.create_table();
-        obj.frame.add(table);
-        obj.frame.pack();
-        JButton submit = new JButton("Submit");
-        obj.frame.add(submit);
-        //submit.addActionListener(this);
-        obj.frame.setVisible(true);
-    }
+		int total = 0;
+		for (int i = 0; i < scores.length; i++)
+			for (int j = 0; j < scores[0].length; j++)
+				total = total + scores[i][j];
 
-/*
-    public void actionPerformed(ActionEvent arg0) {
-        //System.out.println(membersCount + " " + flag);
-        this.frame.setVisible(false);
+		for (int i = 0; i < scores.length; i++) {
+			int current_sum = 0;
 
-        String s = comboBox.getSelectedItem().toString();
-        int x = Integer.parseInt(s);
-        boolean flag = checkbox.isSelected();
-        screen2 obj = new screen2(x,flag);
-        obj.createAndShowGUI();
-    
+			for (int j = 0; j < scores[0].length; j++)
+				current_sum = current_sum + scores[i][j];
 
-    }
-*/ 
-    
+			normalized_score[i] = current_sum / total;
+
+		}
+
+		return normalized_score;
+
+	}
+
+	/*
+	 * public void actionPerformed(ActionEvent arg0) {
+	 * //System.out.println(membersCount + " " + flag);
+	 * this.frame.setVisible(false); String s =
+	 * comboBox.getSelectedItem().toString(); int x = Integer.parseInt(s);
+	 * boolean flag = checkbox.isSelected(); screen2 obj = new screen2(x,flag);
+	 * obj.createAndShowGUI();
+	 * 
+	 * }
+	 */
+
 }
